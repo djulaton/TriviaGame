103768622944456ece69c3bc30865c98a3e3ec6d
Diana <dianajulaton@gmail.com>
2018-06-21 22:47:10 -0700

    reset form

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,41 +8,102 @@
 
 
 $(document).ready(function(){
-// create global variables 
+    // create global variables 
+    
+    var correctAnswers = 0;
+    var incorrectAnswers = 0;
+    var missedQuestions;
+    
+    //event for hiding form and stats on page load
+    
+    $("#game").hide();
+    $("#stats").hide();
+    $("#replayButton").hide();
+    $("#submitButton").hide();
+    
+//create objects for each question with possible answers
+//correct answer will be indicated by index
+//create an array 
 
-var correctAnswers;
-var incorrectAnswers;
+var question1 = {
+    question: "What was Angelica's doll's name",
+    answers: ["Reptar", "Cynthia", "Lillian", "Didi"],
+    correctAnswer: 1,
+};
 
-//event for hiding form and stats on page load
+var question2 = {
+    question: "Who was Doug Funnie's crush?",
+    answers: ["Mrs. Wingo", "Patty Mayonnaise", "BeeBe Bluff", "Loretta LaQuigley"],
+    correctAnswer: 1,
+};
 
-$("#game").hide();
-$("#stats").hide();
-$("#replayButton").hide();
-$("#submitButton").hide();
+var question3 = {
+    question: "Who was Arnold's sidekick?",
+    answers: ["Helga Pataki", "Harold Berman", "Eugene Horowitz", "Gerald Johanssen"],
+    correctAnswer: 3,
+};
 
-//create 30 second timer
+var question4 = {
+    question: "What was Stimpy's favorite song?",
+    answers: ["Lady in red", "Happy, happy, joy, joy", "Mr. Roboto", "Thriller"],
+    correctAnswer: 1,
+};
 
-var counter = 5;
+var question5 = {
+    question: "What was Rocko's pet's name?",
+    answers: ["Dilbert", "Spunky", "Heffer", "Mr. Bighead"],
+    correctAnswer: 1,
+};
 
+var questionsArray = [question1, question2, question3, question4, question5];
+
+
+// tally correct answers
+
+// $("select").each(function(){
+//     if($(this).val()=='right'){ 
+//         correctAnswers++;
+//         alert('right answers: ' + correctAnswers);    
+//     }
+// });
+
+
+// $('#submitButton').click(function() {
+//     var selValue = $('input[optradio=correct]:checked').val(); 
+//     $("#correct").html(selValue);
+// });
+
+
+
+//create a 15 second timer
+
+var counter;
+    
 function timer() {
-    setTimeout(timer, 1000);
-    $('#counter').text(counter);
-    counter--;
-
-    if (counter < 0) {
-        counter = 0;
+    var sec = 15
+    clearInterval(counter);
+    counter = setInterval(function() {
+    $('#counter').text(sec--);
+    if (sec == -1) {
+        clearInterval(counter);
+        $("#submitButton").hide();
         $("#game").hide();
         $("#stats").show();
-        $("#submitButton").hide();
         $("#replayButton").show();
-        counter = 5;
     }
+} , 1000);
 
 }
 
-// timer();
+//create a function to reset form
 
-//create a function for the start page
+function resetForm() {
+    $("#myForm1")[0].reset()
+    $("#myForm2")[0].reset()
+    $("#myForm3")[0].reset()
+    $("#myForm4")[0].reset()
+    $("#myForm5")[0].reset()
+}
 
 //click events
 
@@ -61,6 +122,7 @@ $("#submitButton").on("click", function(){
     $("#game").hide();
     $("#stats").show();
     $("#replayButton").show();
+    timer();
 });
 
 $("#replayButton").on("click", function(){
@@ -68,7 +130,26 @@ $("#replayButton").on("click", function(){
     $("#game").show();
     $("#submitButton").show();
     $("#stats").hide();
+    timer();
+    resetForm();
+    // $("#myForm1")[0].reset()
+    // $("#myForm2")[0].reset()
+    // $("#myForm3")[0].reset()
+    // $("#myForm4")[0].reset()
+    // $("#myForm5")[0].reset()
 });
+
+
+
+// when user answers a question right, increment correct answer score
+// when incorrect or missed question, increment score for incorrect/missed
+// function correct() {
+//     if()
+//     correctAnswers++;
+// }
+
+
+// Put it all on the screen!!! :)
 
 // $("#submitButton").prepend("<button>Submit</button>");
 
@@ -76,4 +157,3 @@ $("#startButton").prepend("<button>Start Game</button>");
 
 $("#replayButton").prepend("<button>Try Again</button>");
 });
-
